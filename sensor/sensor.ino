eb054109ec560ebfab08aa982576a6ed8662543c
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-11-06 17:59:47 +0100

    Set up pins for sensor

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,12 +1,14 @@
-#define LED_BUILTIN 1
+#define RADIO_PIN 0
+#define LED_PIN 1
+#define SWITCH_PIN 2
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  // Light LED when window closed
+  bool windowOpen = digitalRead(SWITCH_PIN);
+  digitalWrite(LED_PIN, !windowOpen);
 }
