6e49915dd63eb91625158972d55c19b4eac16c00
Krzysztof Antoniak <krzysztof@antoniak.in>
2020-11-06 23:09:57 +0100

    Switch to RCSwitch for transmitting

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,26 +1,19 @@
-#include <VirtualWire.h>
+#include <RCSwitch.h>
 
 // Pins
-#define TRANSMIT_PIN 0
+#define TRANSMIT_PIN 3
 #define LED_PIN 1
 #define SWITCH_PIN 2
 
 // Radio communications
-#define BITS_PER_SECOND 2000
-uint8_t message_buffer[4];
+const uint8_t device_id = 0x8C;
+RCSwitch transmitter = RCSwitch();
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(SWITCH_PIN, INPUT_PULLUP);
 
-  vw_set_tx_pin(TRANSMIT_PIN);
-  vw_setup(BITS_PER_SECOND);
-  
-  // Device ID?
-  message_buffer[0] = 0b0001;
-  message_buffer[1] = 0b0011;
-  message_buffer[2] = 0b0111;
-  message_buffer[3] = 0b1111;
+  transmitter.enableTransmit(TRANSMIT_PIN);
 }
 
 void loop() {
@@ -32,8 +25,7 @@ void loop() {
 
   // Send device ID
   digitalWrite(LED_PIN, HIGH);
-  vw_send(message_buffer, sizeof(message_buffer));
-  vw_wait_tx();
+  transmitter.send(device_id, 8);
   digitalWrite(LED_PIN, LOW);
   delay(1000);
 }
