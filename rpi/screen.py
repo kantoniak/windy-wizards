a2052b376f1998a823afd1d7e220822c13b79290
Jadw1 <jadw1_1@o2.pl>
2020-11-07 01:51:24 +0100

    displaying basic info on screen

---
+++ rpi/screen.py
@@ -0,0 +1,73 @@
+# -*- coding:utf-8 -*-
+
+from luma.core.interface.serial import i2c, spi
+from luma.core.render import canvas
+from luma.core import lib
+
+from luma.oled.device import sh1106
+import RPi.GPIO as GPIO
+
+import time
+import subprocess
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = 128
+height = 64
+image = Image.new('1', (width, height))
+
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+RST = 25
+CS = 8		
+DC = 24
+
+USER_I2C = 0
+
+if  USER_I2C == 1:
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(RST,GPIO.OUT)	
+	GPIO.output(RST,GPIO.HIGH)
+	
+	serial = i2c(port=1, address=0x3c)
+else:
+	serial = spi(device=0, port=0, bus_speed_hz = 8000000, transfer_size = 4096, gpio_DC = 24, gpio_RST = 25)
+
+device = sh1106(serial, rotate=2) #sh1106  
+
+try:
+	while True:
+		with canvas(device) as draw:
+			
+			opened = True
+			temp = -40.4
+			hum = 40.1
+
+			lines = [
+				f'Hello, there!',
+				f'Windows is: {"OPENED" if opened else "CLOSED"}',
+				f'Temperature: {temp}ºC',
+				f'Humidity: {hum}%',
+			]
+
+			y = top
+			step = 8
+			for line in lines:
+				draw.text((x, y), line, font=font, fill=255)
+				y += step
+
+except:
+	print("except")
+GPIO.cleanup()
